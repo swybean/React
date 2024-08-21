34e6c10cd58bc93fbf7dc19360ac74b058852d31
swybean <swybean@gmail.com>
2024-08-21 13:26:46 +0900

    리액트를 다루는 기술_todo 미니 프로젝트

---
+++ book_prac/src/App.js
@@ -0,0 +1,74 @@
+import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import { useState, useRef, useCallback } from 'react';
+
+const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링 하기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정 관리 앱을 만들어보자',
+      checked: false,
+    },
+  ]);
+
+  // 일정 추가 기능
+  // 고유값으로 사용될 id
+  // ref를 사용하여 변수를 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  // 일정 삭제 기능
+  // todos 배열에서 id로 항목 지우기
+  const onRemove = useCallback(
+    (id) => {
+      setTodos(todos.filter((todo) => todo.id !== id));
+    },
+    [todos],
+  );
+
+  // 일정 수정 기능
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
+
+export default App;
